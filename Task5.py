11d86939a118fcb31117665ada5430726e845a39
Valeentin87 <Valeentin87@list.ru>
2022-10-18 02:38:16 +0300

    завершающий комит

--- Task5.py
+++ Task5.py
@@ -3,6 +3,5 @@ import random
 N = int(input('введите число элементов в последовательности '))
 lst = [random.randint(0,20) for i in range(N)]
 print(lst)
-#res_lst = [lambda i, j = i-(2*i+1): swap(i, j, lst) (i,) for i in range(N)]
 res_lst = [ lst[i-(2*i+1)] for i in range(N)]
 print(res_lst)
